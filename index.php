e35d551cbdc44dac9100fb2bf844176c2c540dc5
Dedy Haris Setiawan <srcomp.tanjung@gmail.com>
2019-10-06 09:42:58 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+
+    <!-- font -->
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body id="page-top">
+    
+      <!-- <nav class="navbar navbar-light bg-success fixed-top" id="mainNav"> -->
+      <nav class="navbar navbar-expand-lg navbar-light bg-warning fixed-top">
+        <div class="container">
+
+          <h3><i class="fas fa-cart-plus mr-2 text-danger"></i></h3>
+          <a class="navbar-brand font-weight-bold" href="#">SRCOMP</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav ml-auto mr-4">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Reseller <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Hubungi Kami <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Bantuan <span class="sr-only">(current)</span></a>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0 mr-4" type="submit">Search</button>
+            </form>
+            <div class="icon mt-2">
+              <h5>
+                <i class="fas fa-cart-plus mr-3"></i>
+                <i class="fas fa-envelope mr-3"></i>
+                <i class="fas fa-bell mr-3"></i>
+              </h5>
+            </div>
+          </div>
+      </div>
+    </nav>
+
+    <div class="row mt-5 no-gutters">
+      <div class="col-md-2 bg-lights">
+        <ul class="list-group list-group-flush p-2 pt-4">
+          <li class="list-group-item bg-warning"><i class="fas fa-list"></i> KATEGORI PRODUK</li>
+          <li class="list-group-item"><i class="fas fa-angle-right"></i> Peralatan Elektronik</li>
+          <li class="list-group-item"><i class="fas fa-angle-right"></i> Aksesoris Elektronik</li>
+          <li class="list-group-item"><i class="fas fa-angle-right"></i> Fashion Pria</li>
+          <li class="list-group-item"><i class="fas fa-angle-right"></i> Fashion Wanita</li>
+          <li class="list-group-item"><i class="fas fa-angle-right"></i> Fashion Anak</li>
+          <li class="list-group-item"><i class="fas fa-angle-right"></i> Otomotif</li>
+          <li class="list-group-item"><i class="fas fa-angle-right"></i> Hobby & Olahraga</li>
+        </ul>
+      </div>
+      <div class="col-md-10 bg-light mt-4">
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/slide/slide1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="img/slide/slide2.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="img/slide/slide3.jpg" class="d-block w-100" alt="...">
+            </div>
+          </div>
+        </div>
+
+        <h4 class="text-center font-weight-bold m-4">Produk Terbaru</h4>
+
+        <div class="row mx-auto">
+          <div class="card mr-2 ml-2" style="width: 16rem;">
+            <img src="img/produk/kamera.jpg" class="card-img-top" alt="...">
+            <div class="card-body bg-light">
+              <h5 class="card-title">Kamera</h5>
+              <p class="card-text">Kamera CANON EOS 1100D</p>
+              <i class="fas fa-star text-success"></i>
+              <i class="fas fa-star text-success"></i>
+              <i class="fas fa-star text-success"></i>
+              <i class="fas fa-star-half-alt text-success"></i>
+              <i class="far fa-star text-success"></i><br>
+              <a href="#" class="btn btn-primary">Detail</a>
+              <a href="#" class="btn btn-danger">Rp. 5.600.000,-</a>
+            </div>
+          </div>
+
+          <div class="card mr-2 ml-2" style="width: 16rem;">
+            <img src="img/produk/headphone.jpg" class="card-img-top" alt="...">
+            <div class="card-body bg-light">
+              <h5 class="card-title">Headphone</h5>
+              <p class="card-text">Headphone Wireless</p>
+              <i class="fas fa-star text-success"></i>
+              <i class="fas fa-star text-success"></i>
+              <i class="fas fa-star text-success"></i>
+              <i class="fas fa-star-half-alt text-success"></i>
+              <i class="far fa-star text-success"></i><br>
+              <a href="#" class="btn btn-primary">Detail</a>
+              <a href="#" class="btn btn-danger">Rp. 300.000,-</a>
+            </div>
+          </div>
+
+          <div class="card mr-2 ml-2" style="width: 16rem;">
+            <img src="img/produk/laptop.jpg" class="card-img-top" alt="...">
+            <div class="card-body bg-light">
+              <h5 class="card-title">ASUS X441CA</h5>
+              <p class="card-text">Laptop terbagus</p>
+              <i class="fas fa-star text-success"></i>
+              <i class="fas fa-star text-success"></i>
+              <i class="fas fa-star text-success"></i>
+              <i class="fas fa-star-half-alt text-success"></i>
+              <i class="far fa-star text-success"></i><br>
+              <a href="#" class="btn btn-primary">Detail</a>
+              <a href="#" class="btn btn-danger">Rp. 7.500.000,-</a>
+            </div>
+          </div>
+
+          <div class="card mr-2 ml-2" style="width: 16rem;">
+            <img src="img/produk/speaker.jpg" class="card-img-top" alt="...">
+            <div class="card-body bg-light">
+              <h5 class="card-title">SPEAKER</h5>
+              <p class="card-text">Speaker terbaik</p>
+              <i class="fas fa-star text-success"></i>
+              <i class="fas fa-star text-success"></i>
+              <i class="fas fa-star text-success"></i>
+              <i class="fas fa-star-half-alt text-success"></i>
+              <i class="far fa-star text-success"></i><br>
+              <a href="#" class="btn btn-primary">Detail</a>
+              <a href="#" class="btn btn-danger">Rp. 100.000,-</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
+  </body>
+</html>
